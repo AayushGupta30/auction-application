54fdf5343fc631ee2bd60415f8ca230918edb5db
Aayush Gupta <aayush.gupta3009@gmail.com>
2026-01-31 19:40:32 +0530

    final

--- auction-backend/server.js
+++ auction-backend/server.js
@@ -22,7 +22,8 @@ const CORS_ORIGIN = process.env.CORS_ORIGIN || "*";
 // MIDDLEWARE
 // ===============================
 app.use(cors({ origin: CORS_ORIGIN }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+
 
 // ===============================
 // BASIC AUTH
